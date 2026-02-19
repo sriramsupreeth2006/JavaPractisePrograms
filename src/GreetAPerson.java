389bfcfd5fbf84666474279c42fb91119df89844
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2026-02-19 21:33:41 +0530

    Greet A Person Code

---
+++ src/GreetAPerson.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class GreetAPerson {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        System.out.println("Hello " + S + "!");
+    }
+}
